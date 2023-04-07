70bb01dde91720900e81e8a463c1d390d5960dcd
ReXerses <108536533+ReXerses@users.noreply.github.com>
2023-04-07 22:48:56 +0200

    Btn fix, makeGrid(Custom size)/deleteGrid function added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,44 @@
-//<label id="size-canvas" for="canvas-size"></label>
+const sizeCanvas = document.querySelector("#size-canvas");
+const canvasDimensionC = document.querySelector("#canvasDimension");
+const container = document.querySelector('.canvas');
+const btnNew = document.querySelector('#newCanvas') ;
+
+
+function makeGrid() {
+    for (i = 0; i < canvasDimensionC.value; i++) {
+      const row = document.createElement("div");
+      row.className = "row";
+      container.appendChild(row);
+
+      for (j = 0; j < canvasDimensionC.value; j++) {
+        const col = document.createElement("div");
+        col.className = "col";
+        row.appendChild(col);
+
+      }
+    }
+}
+
+function deleteGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+
+
+
+btnNew.addEventListener('click', () => {
+    
+    deleteGrid();
+    makeGrid();
+    
+});
+
+
+sizeCanvas.textContent = canvasDimensionC.value; //label number default
+
+canvasDimensionC.addEventListener("input", (e) => {
+  sizeCanvas.textContent = e.target.value;
+})
